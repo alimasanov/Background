0b48712ebac7eef2399cb17bbc7094eb8519859b
Alim Asanov <a.asanov@ideas-world.com>
2021-04-21 13:06:33 +0300

    bitmap configure functions

--- app/src/main/java/com/iwgroup/background/Background.kt
+++ app/src/main/java/com/iwgroup/background/Background.kt
@@ -6,17 +6,12 @@ import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
 import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.DrawableRes
 import androidx.annotation.IntRange
 import androidx.core.content.res.ResourcesCompat
 
 class Background(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     View(context, attrs, defStyleAttr) {
-
-    init {
-        layoutParams = ViewGroup.LayoutParams(180.toPx(), 110.toPx())
-    }
 
     override fun onDraw(canvas: Canvas) {
         canvas.drawRoundRectFromDrawable(
@@ -40,36 +35,24 @@ class Background(context: Context, attrs: AttributeSet? = null, defStyleAttr: In
         val rect = Rect(0, 0, _width, _height)
         val rectF = RectF(rect)
         path.addRoundRect(rectF, angleRadius, angleRadius, Path.Direction.CW)
+        clipPath(path)
         bitmap?.let {
-            clipPath(path)
             drawBitmap(it, rect, rect, Paint(Paint.ANTI_ALIAS_FLAG))
         }
     }
 
-    fun Int.toPx(): Int = (toFloat() * Resources.getSystem().displayMetrics.density).toInt()
 
-    fun Context.getBitmap(id: Int, _width: Int, _height: Int): Bitmap? {
-        ResourcesCompat.getDrawable(resources, id, theme)?.let {
-            if (it is BitmapDrawable) return it.bitmap
-            val bitmap = Bitmap.createBitmap(_width, _height, Bitmap.Config.ARGB_8888)
-            val canvas = Canvas(bitmap)
-            it.setBounds(0, 0, canvas.width, canvas.height)
-            it.draw(canvas)
-            return bitmap
-        }
-        return null
-    }
+}
+fun Int.toPx(): Int = (toFloat() * Resources.getSystem().displayMetrics.density).toInt()
 
-    fun Bitmap.makeTransparent(@IntRange(from = 0, to = 100) value: Int): Bitmap {
-        val alpha = 255 * value / 100
-        val _width = width
-        val _height = height
-        val transBitmap = Bitmap.createBitmap(_width, _height, Bitmap.Config.ARGB_8888)
-        val canvas = Canvas(transBitmap)
-        canvas.drawARGB(0, 0, 0, 0)
-        val paint = Paint()
-        paint.alpha = alpha
-        canvas.drawBitmap(this, 0f, 0f, paint)
-        return transBitmap
+fun Context.getBitmap(id: Int, _width: Int, _height: Int): Bitmap? {
+    ResourcesCompat.getDrawable(resources, id, theme)?.let {
+        if (it is BitmapDrawable) return it.bitmap
+        val bitmap = Bitmap.createBitmap(_width, _height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        it.setBounds(0, 0, canvas.width, canvas.height)
+        it.draw(canvas)
+        return bitmap
     }
+    return null
 }
--- app/src/main/java/com/iwgroup/background/MainActivity.kt
+++ app/src/main/java/com/iwgroup/background/MainActivity.kt
@@ -1,11 +1,95 @@
 package com.iwgroup.background
 
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.SeekBar
+import androidx.annotation.IntRange
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val SAVED_ALPHA = "SAVED_ALPHA"
+        const val SAVED_CORNER = "SAVED_CORNER"
+    }
+
+    private var savedAlpha: Int = 0
+    private var savedCorner: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setImage(savedAlpha, savedCorner)
+
+        findViewById<SeekBar>(R.id.sbCorners).setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser && progress in 0..50) {
+                    savedCorner = progress
+                    setImage(savedAlpha, progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        findViewById<SeekBar>(R.id.sbAlpha).setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser && progress in 0..100) {
+                    savedAlpha = progress
+                    setImage(progress, savedCorner)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
+
+    private fun setImage(@IntRange(from = 0, to = 100) alpha: Int, cornersDp: Int = 8) {
+        findViewById<ImageView>(R.id.ivImage)
+            .setImageBitmap(
+                applicationContext
+                    .getBitmap(
+                        R.drawable.ic__021,
+                        180.toPx(),
+                        110.toPx()
+                    )
+                    ?.makeTransparent(alpha)
+                    ?.makeRoundedCorners(cornersDp.toPx().toFloat())
+            )
+    }
+
+}
+
+
+
+fun Bitmap.makeTransparent(@IntRange(from = 0, to = 100) value: Int): Bitmap {
+    val alpha = 255 * value / 100
+    val _width = width
+    val _height = height
+    val transBitmap = Bitmap.createBitmap(_width, _height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(transBitmap)
+    val paint = Paint()
+    paint.alpha = alpha
+    canvas.drawARGB(0, 0, 0, 0)
+    canvas.drawBitmap(this, 0f, 0f, paint)
+    return transBitmap
+}
+
+fun Bitmap.makeRoundedCorners(angleRadius: Float): Bitmap {
+    val path = Path()
+    val bitmap = this
+    val transBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(transBitmap)
+    val rect = Rect(0, 0, width, height)
+    val rectF = RectF(rect)
+    path.addRoundRect(rectF, angleRadius, angleRadius, Path.Direction.CW)
+    canvas.clipPath(path)
+    canvas.drawBitmap(bitmap, rect, rect, Paint(Paint.ANTI_ALIAS_FLAG))
+    return transBitmap
 }
